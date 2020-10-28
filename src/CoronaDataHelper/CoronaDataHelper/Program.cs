3a7d6c04a97216cf063ecc6bcb173008df918ad6
Christoph <Christoph.Krause@artec.de>
2020-10-28 12:04:36 +0100

    - Added ireland - Added deaths
    TODO:
    clean up the quick and dirty code

--- src/CoronaDataHelper/CoronaDataHelper/Program.cs
+++ src/CoronaDataHelper/CoronaDataHelper/Program.cs
@@ -50,7 +50,8 @@ namespace CoronaDataHelper {
 
 				Console.WriteLine(oJSONCoronaVirusData.DEU.data.Length);
 
-				processXLSX(strFileNameExcelx, oJSONCoronaVirusData);
+				processXLSXInfected(strFileNameExcelx, oJSONCoronaVirusData);
+				processXLSXDeath(strFileNameExcelx, oJSONCoronaVirusData);
 				//NEXT:
 				//1)Push data from JSON to excel
 				//2) Add formula to XLSX
@@ -58,14 +59,13 @@ namespace CoronaDataHelper {
 				Console.WriteLine("Error:" + e);
 			}
 		}
-
-		private static void processXLSX(string strFileNameExcelx, JSONCoronaVirusData result) {
+		private static void processXLSXDeath(string strFileNameExcelx, JSONCoronaVirusData result) {
 			//XLSX Layout:
 			//Date	Italy	Spain 	USA 	Germany	France	Iran	UK	Netherlands	Belgium	Sweden	Brazil
 			//Date ITA ESP USA  DEU  FRA  IRN  GBR NLD BEL SWE BRA
 
 			Console.WriteLine("OPEN Excel " + strFileNameExcelx);
-			SLDocument sl = new SLDocument(strFileNameExcelx, "current");
+			SLDocument sl = new SLDocument(strFileNameExcelx, "death");
 			//validate
 			var strValue = sl.GetCellValueAsString("E1");
 			validateFile(sl);
@@ -73,77 +73,168 @@ namespace CoronaDataHelper {
 			//	Console.WriteLine("strValue:" + strValue);
 			//Console.WriteLine(result.DEU.data[1].date + ":" + result.DEU.data[1].new_cases);
 
-			setData(sl, result.ITA,"B");
-			setData(sl, result.ESP, "C");
-			setData(sl, result.USA, "D");
-			setData(sl, result.DEU, "E");
-			setData(sl, result.FRA, "F");
-			setData(sl, result.IRN, "G");
-			setData(sl, result.GBR, "H");
-			setData(sl, result.NLD, "I");
-			setData(sl, result.BEL, "J");
-			setData(sl, result.SWE, "K");
-			setData(sl, result.BRA, "L");
+			setData(sl, result.ITA, "B", ERow.death);
+			setData(sl, result.ESP, "C", ERow.death);
+			setData(sl, result.USA, "D", ERow.death);
+			setData(sl, result.DEU, "E", ERow.death);
+			setData(sl, result.FRA, "F", ERow.death);
+			setData(sl, result.IRN, "G", ERow.death);
+			setData(sl, result.GBR, "H", ERow.death);
+			setData(sl, result.NLD, "I", ERow.death);
+			setData(sl, result.BEL, "J", ERow.death);
+			setData(sl, result.SWE, "K", ERow.death);
+			setData(sl, result.BRA, "L", ERow.death);
+			setData(sl, result.IRL, "M", ERow.death);
 
-			sl.SetCellValue("E2", (int)result.DEU.data[1].new_cases + "");
+	
 			Console.WriteLine("save Excel " + strFileNameExcelx);
 
 			sl.Save();
 		}
-		private static void setData(SLDocument sl, ESP oESP, String strColumn) {
+
+		internal enum ERow {
+			death,
+			infected
+		}
+		private static void processXLSXInfected(string strFileNameExcelx, JSONCoronaVirusData result) {
+			//XLSX Layout:
+			//Date	Italy	Spain 	USA 	Germany	France	Iran	UK	Netherlands	Belgium	Sweden	Brazil
+			//Date ITA ESP USA  DEU  FRA  IRN  GBR NLD BEL SWE BRA
+
+			Console.WriteLine("OPEN Excel " + strFileNameExcelx);
+			SLDocument sl = new SLDocument(strFileNameExcelx, "infected");
+			//validate
+			var strValue = sl.GetCellValueAsString("E1");
+			validateFile(sl);
+
+			//	Console.WriteLine("strValue:" + strValue);
+			//Console.WriteLine(result.DEU.data[1].date + ":" + result.DEU.data[1].new_cases);
+
+			setData(sl, result.ITA,"B", ERow.infected);
+			setData(sl, result.ESP, "C", ERow.infected);
+			setData(sl, result.USA, "D", ERow.infected);
+			setData(sl, result.DEU, "E", ERow.infected);
+			setData(sl, result.FRA, "F", ERow.infected);
+			setData(sl, result.IRN, "G", ERow.infected);
+			setData(sl, result.GBR, "H", ERow.infected);
+			setData(sl, result.NLD, "I", ERow.infected);
+			setData(sl, result.BEL, "J", ERow.infected);
+			setData(sl, result.SWE, "K", ERow.infected);
+			setData(sl, result.BRA, "L", ERow.infected);
+			setData(sl, result.IRL, "M", ERow.infected);
+
+			
+			Console.WriteLine("save Excel " + strFileNameExcelx);
+
+			sl.Save();
+		}
+		private static void setData(SLDocument sl, IRL oIRL, String strColumn, ERow eRow) {
+			for (int i = 1; i < oIRL.data.Length; i++) {
+				int iValue = (int)oIRL.data[i].new_cases;
+				if (eRow == ERow.death) {
+					iValue = (int)oIRL.data[i].new_deaths;
+				}
+				sl.SetCellValue(strColumn + "" + (i + 1), iValue + "");
+			}
+		}
+		private static void setData(SLDocument sl, ESP oESP, String strColumn, ERow eRow) {
 			for (int i = 1; i < oESP.data.Length; i++) {
-				sl.SetCellValue(strColumn + "" + (i + 1), (int)oESP.data[i].new_cases + "");
+				int iValue = (int)oESP.data[i].new_cases;
+				if (eRow == ERow.death) {
+					iValue = (int)oESP.data[i].new_deaths;
+				}
+				sl.SetCellValue(strColumn + "" + (i + 1), iValue + "");
 			}
 		}
-		private static void setData(SLDocument sl, USA oUSA, String strColumn) {
+		private static void setData(SLDocument sl, USA oUSA, String strColumn, ERow eRow) {
 			for (int i = 1; i < oUSA.data.Length; i++) {
-				sl.SetCellValue(strColumn + "" + (i + 1), (int)oUSA.data[i].new_cases + "");
+				int iValue = (int)oUSA.data[i].new_cases;
+				if (eRow == ERow.death) {
+					iValue = (int)oUSA.data[i].new_deaths;
+				}
+				sl.SetCellValue(strColumn + "" + (i + 1), iValue + "");
 			}
 		}
-		private static void setData(SLDocument sl, DEU oDEU, String strColumn) {
+		private static void setData(SLDocument sl, DEU oDEU, String strColumn, ERow eRow) {
 			for (int i = 1; i < oDEU.data.Length; i++) {
-				sl.SetCellValue(strColumn + "" + (i + 1), (int)oDEU.data[i].new_cases + "");
+				int iValue = (int)oDEU.data[i].new_cases;
+				if (eRow == ERow.death) {
+					iValue = (int)oDEU.data[i].new_deaths;
+				}
+				sl.SetCellValue(strColumn + "" + (i + 1), iValue + "");
 			}
 		}
-		private static void setData(SLDocument sl, FRA oFRA, String strColumn) {
+		private static void setData(SLDocument sl, FRA oFRA, String strColumn, ERow eRow) {
 			for (int i = 1; i < oFRA.data.Length; i++) {
-				sl.SetCellValue(strColumn + "" + (i + 1), (int)oFRA.data[i].new_cases + "");
+				int iValue = (int)oFRA.data[i].new_cases;
+				if (eRow == ERow.death) {
+					iValue = (int)oFRA.data[i].new_deaths;
+				}
+				sl.SetCellValue(strColumn + "" + (i + 1), iValue + "");
 			}
 		}
-		private static void setData(SLDocument sl, IRN oIRN, String strColumn) {
+		private static void setData(SLDocument sl, IRN oIRN, String strColumn, ERow eRow) {
 			for (int i = 1; i < oIRN.data.Length; i++) {
-				sl.SetCellValue(strColumn + "" + (i + 1), (int)oIRN.data[i].new_cases + "");
+				int iValue = (int)oIRN.data[i].new_cases;
+				if (eRow == ERow.death) {
+					iValue = (int)oIRN.data[i].new_deaths;
+				}
+				sl.SetCellValue(strColumn + "" + (i + 1), iValue + "");
 			}
 		}
-		private static void setData(SLDocument sl, GBR oGBR, String strColumn) {
+		private static void setData(SLDocument sl, GBR oGBR, String strColumn, ERow eRow) {
 			for (int i = 1; i < oGBR.data.Length; i++) {
-				sl.SetCellValue(strColumn + "" + (i + 1), (int)oGBR.data[i].new_cases + "");
+				int iValue = (int)oGBR.data[i].new_cases;
+				if (eRow == ERow.death) {
+					iValue = (int)oGBR.data[i].new_deaths;
+				}
+				sl.SetCellValue(strColumn + "" + (i + 1), iValue + "");
 			}
 		}
-		private static void setData(SLDocument sl, BEL oBEL, String strColumn) {
+		private static void setData(SLDocument sl, BEL oBEL, String strColumn, ERow eRow) {
 			for (int i = 1; i < oBEL.data.Length; i++) {
-				sl.SetCellValue(strColumn + "" + (i + 1), (int)oBEL.data[i].new_cases + "");
+				int iValue = (int)oBEL.data[i].new_cases;
+				if (eRow == ERow.death) {
+					iValue = (int)oBEL.data[i].new_deaths;
+				}
+				sl.SetCellValue(strColumn + "" + (i + 1), iValue + "");
 			}
 		}
-		private static void setData(SLDocument sl, SWE oSWE, String strColumn) {
+		private static void setData(SLDocument sl, SWE oSWE, String strColumn, ERow eRow) {
 			for (int i = 1; i < oSWE.data.Length; i++) {
-				sl.SetCellValue(strColumn + "" + (i + 1), (int)oSWE.data[i].new_cases + "");
+				int iValue = (int)oSWE.data[i].new_cases;
+				if (eRow == ERow.death) {
+					iValue = (int)oSWE.data[i].new_deaths;
+				}
+				sl.SetCellValue(strColumn + "" + (i + 1), iValue + "");
 			}
 		}
-		private static void setData(SLDocument sl, BRA oBRA, String strColumn) {
+		private static void setData(SLDocument sl, BRA oBRA, String strColumn, ERow eRow) {
 			for (int i = 1; i < oBRA.data.Length; i++) {
-				sl.SetCellValue(strColumn + "" + (i + 1), (int)oBRA.data[i].new_cases + "");
+				int iValue = (int)oBRA.data[i].new_cases;
+				if (eRow == ERow.death) {
+					iValue = (int)oBRA.data[i].new_deaths;
+				}
+				sl.SetCellValue(strColumn + "" + (i + 1), iValue + "");
 			}
 		}
-		private static void setData(SLDocument sl, NLD oNLD, String strColumn) {
+		private static void setData(SLDocument sl, NLD oNLD, String strColumn, ERow eRow) {
 			for (int i = 1; i < oNLD.data.Length; i++) {
-				sl.SetCellValue(strColumn + "" + (i + 1), (int)oNLD.data[i].new_cases + "");
+				int iValue = (int)oNLD.data[i].new_cases;
+				if (eRow == ERow.death) {
+					iValue = (int)oNLD.data[i].new_deaths;
+				}
+				sl.SetCellValue(strColumn + "" + (i + 1), iValue + "");
 			}
 		}
-		private static void setData(SLDocument sl, ITA oiTA, String strColumn) {
+		private static void setData(SLDocument sl, ITA oiTA, String strColumn, ERow eRow) {
 			for (int i=1; i< oiTA.data.Length; i++) {
-			//	Console.WriteLine(oiTA.data[i].date + " : " + oiTA.data[i].new_cases);
-				sl.SetCellValue(strColumn+""+(i+1), (int)oiTA.data[i].new_cases + "");
+				//	Console.WriteLine(oiTA.data[i].date + " : " + oiTA.data[i].new_cases);
+				int iValue = (int)oiTA.data[i].new_cases;
+				if (eRow == ERow.death) {
+					iValue = (int)oiTA.data[i].new_deaths;
+				}
+				sl.SetCellValue(strColumn + "" + (i + 1), iValue + "");
 			}
 		
 		}
@@ -194,8 +285,12 @@ namespace CoronaDataHelper {
 				throw new Exception("Invalid File:" + strValue + "!=Sweden");
 			}
 			strValue = sl.GetCellValueAsString("L1").Trim();
-			if (!sl.GetCellValueAsString("L1").Equals("Brazil", StringComparison.InvariantCultureIgnoreCase)) {
+			if (!strValue.Equals("Brazil", StringComparison.InvariantCultureIgnoreCase)) {
 				throw new Exception("Invalid File:" + strValue + "!=Brazil");
+			}
+			strValue = sl.GetCellValueAsString("M1").Trim();
+			if (!strValue.Equals("Ireland", StringComparison.InvariantCultureIgnoreCase)) {
+				throw new Exception("Invalid File:" + strValue + "!=Ireland");
 			}
 		}
 	}
